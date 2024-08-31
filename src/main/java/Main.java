f5f1a54c0039a67e745919202a3a7aec68cc79c6
martinmiro <mmiro578@gmail.com>
2024-08-30 21:41:47 -0300

    prueba 0.0.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.HashMap;
+
+import mycompany.logica.Mesa;
+
 public class Main {
     public static void main(String[] args) {
+        HashMap<Integer, Mesa> mesas = new HashMap<Integer, Mesa>();
 
+        
     }
 }
+
